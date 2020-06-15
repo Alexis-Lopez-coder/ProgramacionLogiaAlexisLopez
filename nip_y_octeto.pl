6405aec6d234e9aea2f91f8b4f314498ded3d2ba
Alexis-Lopez-coder <61531966+Alexis-Lopez-coder@users.noreply.github.com>
2020-06-15 10:41:18 -0500

    Add files via upload
    CORREGIDO

--- nip_y_octeto.pl
+++ nip_y_octeto.pl
@@ -43,12 +43,18 @@ nip(L) :- string_length(L,N), N == 4, preparar_string(L,R), digitos(R).
 % R5 ::= 0 | 1 | 2 | 3 | 4 | 5
 % Digito ::= 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9
 
-validar2(D) :- D == "2".
-validar1(D) :- D == "1".
+validar([F|C],"2"):- r52([F|C]).
+
+validar([F|C],"1") :- digitos([F|C]).
+
 
 r5(X) :- X == "0"; X == "1"; X == "2"; X == "3"; X == "4"; X == "5".
-r52([F|[]]) :- r5(F).
+
 r52([F|C]) :- r5(F), r52(C).
 
+r52([F|[]]) :- r5(F).
 
-octeto(L) :- split_string(L,".","",[F|C]),preparar_string(F,R),[P|O] = R, validar2(P),r52(O), [W|T] = C, preparar_string(W,E), [Q|A] = E, validar1(Q), digitos(A), [Z|V] = T, preparar_string(Z,B), length(B,2),[M | N] = B, digito(M), [ J | Y ] = N, digito(J), [I|S] = V, digito(I).
+
+octeto(L) :- preparar_string(L,R), string_length(L,3), [P|O] = R, validar(R,P).
+octeto(L) :- preparar_string(L,R), string_length(L,2), digitos(R).
+octeto(L) :- preparar_string(L,R), string_length(L,1), digitos(R).
